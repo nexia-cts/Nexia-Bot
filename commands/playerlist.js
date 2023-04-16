f5b6928878217026799677cf63711588bf1df2c6
NotCoded <66999075+not-coded@users.noreply.github.com>
2023-04-16 19:39:47 +0200

    playerlist but more enhanced

--- commands/playerlist.js
+++ commands/playerlist.js
@@ -23,17 +23,32 @@ module.exports = {
         embed.setThumbnail("https://cdn.discordapp.com/icons/1041553022246998087/6a007c32cc01332188bbb3efcab73499.webp?size=80")
         if (region != null && region == "eu") {
             eu = await util.get(`https://api.mcstatus.io/v2/status/java/nexia.mcserver.us:25565`)
-            const euList = eu.data.players.list ? "\n\`\`\`" + eu.data.players.list.map(p => ` ${p.name_clean} `).join('\r\n') + "\`\`\`" : "";
-            embed.setDescription("**EU** Playerlist:" + euList)
+            if (eu.data.players.online > 0) {
+                const euList = na.data.players.list ? "\n\`\`\`" + na.data.players.list.map(p => ` ${p.name_clean} `).join('\r\n') + "\`\`\`" : "";
+                embed.setDescription(`**EU** Playerlist:` + euList)
+            } else {
+                embed.setDescription(`**EU** Playerlist:\n\`\`\`No players\`\`\``)
+            }
         } else if (region != null && region == "na") {
             na = await util.get(`https://api.mcstatus.io/v2/status/java/nanexia.mcserver.us:25565`)
-            const naList = na.data.players.list ? "\n\`\`\`" + na.data.players.list.map(p => ` ${p.name_clean} `).join('\r\n') + "\`\`\`" : "";
-            embed.setDescription("**NA** Playerlist:" + naList)
+            if (na.data.players.online > 0) {
+                const naList = na.data.players.list ? "\n\`\`\`" + na.data.players.list.map(p => ` ${p.name_clean} `).join('\r\n') + "\`\`\`" : "";
+                embed.setDescription(`**NA** Playerlist:` + naList)
+            } else {
+                embed.setDescription(`**NA** Playerlist:\n\`\`\`No players\`\`\``)
+            }
+
         } else {
             eu = await util.get(`https://api.mcstatus.io/v2/status/java/nexia.mcserver.us:25565`)
             na = await util.get(`https://api.mcstatus.io/v2/status/java/nanexia.mcserver.us:25565`)
-            const euList = eu.data.players.list ? "\n\`\`\`" + eu.data.players.list.map(p => ` ${p.name_clean} `).join('\r\n') + "\`\`\`" : "";
-            const naList = na.data.players.list ? "\n\`\`\`" + na.data.players.list.map(p => ` ${p.name_clean} `).join('\r\n') + "\`\`\`" : "";
+            const euList = `\`\`\`No players\`\`\``
+            if (eu.data.players.online > 0) {
+                euList = eu.data.players.list ? "\n\`\`\`" + eu.data.players.list.map(p => ` ${p.name_clean} `).join('\r\n') + "\`\`\`" : "";
+            }
+            const naList = `\`\`\`No players\`\`\``
+            if (na.data.players.online > 0) {
+                naList = na.data.players.list ? "\n\`\`\`" + na.data.players.list.map(p => ` ${p.name_clean} `).join('\r\n') + "\`\`\`" : "";
+            }
             embed.setDescription(`**EU** Playerlist:` + euList + `\n**NA** Playerlist:` + naList)
         }
 
