635fa7fd14e51639a21547fa4eeea8c7a50cc8b3
NotCoded <66999075+not-coded@users.noreply.github.com>
2023-08-04 22:36:24 +0200

    add rule

--- index.js
+++ index.js
@@ -100,7 +100,8 @@ client.on(Events.InteractionCreate, async interaction => {
                     { name: '**4. No encouraging of illegal activity.**', value: 'Self-explanatory.' },
                     { name: '**5. No interrupting other player\'s fights.**', value: 'Please do not interrupt other player\'s fights, focusing on killing certain players is not allowed too.' },
                     { name: '**6. No spamming/filling up chat**', value: 'Please do not spam the chat, nor fill up the chat.' },
-                    { name: '**7. No stalling/combat logging.**', value: 'You may not stall (running away for a long time), or combat log (logging out when you\'re low).' }
+                    { name: '**7. No stalling/combat logging.**', value: 'You may not stall (running away for a long time), or combat log (logging out when you\'re low).' },
+                    { name: '**8. No bedrock bridging**', value: 'You may not use bedrock bridging or reach-around bridging, **NO EXCEPTIONS**.' }
                 )
             }
 
